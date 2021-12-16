3cdec23eb086445b212a6d032998a58a3e476baa
Jonas Daniels <jonas.daniels@outlook.com>
2021-12-16 14:52:17 -0800

    add some comments

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,9 @@
 import { useEffect, useState, useMemo } from "react";
 //ethers is a utility library for interacting with the Ethereum blockchain, we'll be using it later
 import { ethers } from "ethers";
-// we import the ThirdwebSDK from the thirdweb sdk library, we'll use this
+// we import the ThirdwebSDK from the thirdweb sdk library, we'll use this to interact with our modules that we deploy in the scripts
 import { ThirdwebSDK } from "@3rdweb/sdk";
-// we import the useWeb3 hook from the thirdweb hooks library
+// we import the useWeb3 hook from the thirdweb hooks library, we'll use this to connect to the blockchain
 import { useWeb3 } from "@3rdweb/hooks";
 // this is just a little helper function to format long wallet addresses into something more readable, we'll use it later on
 const shortenAddress = (str) => {

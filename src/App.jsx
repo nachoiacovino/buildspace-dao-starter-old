a37aed352afe40919406faed322e0baede10698e
Jonas Daniels <jonas.daniels@outlook.com>
2021-12-16 12:09:10 -0800

    cleanup starting point a bit

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,9 @@
-import "./App.css";
-import { useEffect, useState, useMemo } from "react";
-// we import the useWeb3 hook from the thirdweb hooks library
-import { useWeb3 } from "@3rdweb/hooks";
-// we import the ThirdwebSDK from the thirdweb sdk library
-import { ThirdwebSDK } from "@3rdweb/sdk";
-import { ethers } from "ethers";
-
-const shortenAddress = (str) => {
-  return str.substring(0, 6) + "..." + str.substring(str.length - 4);
-};
-
-// we instatiate the sdk with the Alchemy API URL or rinkeby network name
-const alchemyApiUrl = "";
-const sdk = new ThirdwebSDK(alchemyApiUrl || "rinkeby");
-
 const App = () => {
   return (
     <div className="container mx-auto my-auto align-center flex column text-center">
       <h1>Welcome to 🍪DAO</h1>
-    </div>);
+    </div>
+  );
 };
 
 export default App;

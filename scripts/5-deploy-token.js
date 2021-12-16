dc2902472d13aabcfd0da3e80852f84ce9118c19
Nacho Iacovino <nacho.iacovino@gmail.com>
2021-12-16 14:42:57 +0100

    token

--- scripts/5-deploy-token.js
+++ scripts/5-deploy-token.js
@@ -1,5 +1,8 @@
-//we are importing the sdk that we previously set up in the "initialize-sdk.js" file
-import * as sdk from "./initialize-sdk";
+// we are importing the sdk that we previously set up in the "1-initialize-sdk.js" file
+import sdk from "./1-initialize-sdk.js";
+
+// in order to deploy the new module we need our old friend the app module again.
+const app = sdk.getAppModule("<APP_MODULE>");
 
 /**
  *

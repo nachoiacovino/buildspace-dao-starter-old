5f9da12b98c50b96a9ca205810bb79861a249e98
Nacho Iacovino <nacho.iacovino@gmail.com>
2021-12-16 12:22:46 +0100

    sdk templatw

--- scripts/1-initialize-sdk.js
+++ scripts/1-initialize-sdk.js
@@ -1,9 +1,11 @@
 import { ThirdwebSDK } from "@3rdweb/sdk";
+import { ethers } from "ethers";
 //Importing and configuring our .env file that we use to securely store our environment variables
 import dotenv from "dotenv";
 dotenv.config();
 
-// your code to create the SDK will go here
+// Initialize the SDK
 const sdk = null;
 
+//we are exporting the initialized thirdweb SDK so that we can use it in our other scripts
 export default sdk;

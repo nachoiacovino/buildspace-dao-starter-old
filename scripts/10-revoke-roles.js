48cef89b1a8f6908cd0027dac4f93d462ba56a5c
Nacho Iacovino <nacho.iacovino@gmail.com>
2021-12-16 19:05:01 +0100

    add imports to scripts 8-10

--- scripts/10-revoke-roles.js
+++ scripts/10-revoke-roles.js
@@ -1,5 +1,16 @@
-//we are importing the sdk that we previously set up in the "initialize-sdk.js" file
-import * as sdk from "./initialize-sdk";
+// we are importing the sdk that we previously set up in the "1-initialize-sdk.js" file
+import sdk from "./1-initialize-sdk.js";
+
+// okay, we have everything set up right? what's left?
+// there is one small thing that we *should* do in order for members to be able to be able to trust that the DAO is legit
+// we need to *remove* our own access to the DAO treasury, it is now set up, it should only be manipulated by the DAO members through voting
+
+// ⚠️⚠️⚠️ PLEASE NOTE that once you run this script you WILL NOT be able to to interact with the DAO treasury anymore, you will only be able to interact with the treasury through the voting
+
+// one last time we'll grab our token module
+const tokenModule = sdk.getTokenModule(
+  "<TOKEN_MODULE_ADDRESS>",
+);
 
 /**
  *
--- scripts/8-deploy-vote.js
+++ scripts/8-deploy-vote.js
@@ -1,5 +1,10 @@
-//we are importing the sdk that we previously set up in the "initialize-sdk.js" file
-import * as sdk from "./initialize-sdk";
+// we are importing the sdk that we previously set up in the "1-initialize-sdk.js" file
+import sdk from "./1-initialize-sdk.js";
+
+// in order to deploy the new module we need our old friend the app module for one last time.
+const appModule = sdk.getAppModule(
+  "<APP_MODULE_ADDRESS>",
+);
 
 /**
  *

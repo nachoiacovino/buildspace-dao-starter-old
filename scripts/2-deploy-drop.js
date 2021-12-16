2dcdcbfbd65860ffa53721e2bf10556e777abaab
Nacho Iacovino <nacho.iacovino@gmail.com>
2021-12-16 13:20:06 +0100

    deploy drop imports

--- scripts/2-deploy-drop.js
+++ scripts/2-deploy-drop.js
@@ -1,5 +1,11 @@
-//we are importing the sdk that we previously set up in the "initialize-sdk.js" file
-import * as sdk from "./initialize-sdk";
+import { ethers } from "ethers";
+//we are importing the sdk that we previously set up in the "1-initialize-sdk.js" file
+import sdk from "./1-initialize-sdk.js";
+//we use readFileSync to read the image file that we want to use for our drop
+import { readFileSync } from "fs";
+
+//in order to deploy the new module we need to first know which app we are deploying it to
+const app = sdk.getAppModule("<YOUR-APP-MODULE>");
 
 /**
  *

7ef8497e33140a574afbd95b69820ee116c1f443
Nacho Iacovino <nacho.iacovino@gmail.com>
2021-12-16 14:47:54 +0100

    add imports

--- scripts/6-print-money.js
+++ scripts/6-print-money.js
@@ -1,5 +1,18 @@
-//we are importing the sdk that we previously set up in the "initialize-sdk.js" file
-import * as sdk from "./initialize-sdk";
+//ethers is an amazing library that has a bunch of helpful functions to do blockchain things
+import { ethers } from "ethers";
+// we are importing the sdk that we previously set up in the "1-initialize-sdk.js" file
+import sdk from "./1-initialize-sdk.js";
+
+/**
+ * ------------------------------------------------
+ * 💸💸💸💸💸💸 LETS PRINT SOME FAKE MONEY 💸💸💸💸💸💸
+ * ------------------------------------------------
+ */
+
+// in order to print our fake money we need to first get the token module that we deployed in the previous script
+const tokenModule = sdk.getTokenModule(
+  "<TOKEN_MODULE_ADDRESS>",
+);
 
 /**
  *
